31c80afd043a38016422ae3877200b4f30a048d8
Hadiru Colley <dirudeen22@gmail.com>
2023-09-29 15:50:45 +0300

    modify div content

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,50 @@
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  Nav,
+  PopularProducts,
+  SuperQuality,
+  SpecialOffer,
+  Services,
+  Subscribe,
+} from "./sections/index";
 
 const App = () => (
   <main className="relative">
-
-    Nav
-
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
-      hero
+      <Hero />
     </section>
 
     <section className="padding">
-      PopularProducts
+      <PopularProducts />
     </section>
 
     <section className="padding">
-      SuperQuality
+      <SuperQuality />
     </section>
 
     <section className="padding-x py-10">
-      Services
+      <Services />
     </section>
 
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer />
     </section>
 
     <section className="bg-pale-blue padding">
-      CustomerReviews
+      <CustomerReviews />
     </section>
 
     <section className="padding-x sm:py-32 py-16 w-full">
-      Subcribe
+      <Subscribe />
     </section>
 
     <section className="bg-black padding-x padding-t pb-8">
-      footer
+      <Footer />
     </section>
-
-
   </main>
-)
+);
 
-
-export default App
+export default App;
