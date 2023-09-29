4c9ba80ed4eb993cf9338e4783b255f57c16f004
Hadiru Colley <dirudeen22@gmail.com>
2023-09-29 16:37:39 +0300

    Add logo and links to the navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,28 @@
+import { headerLogo } from "../assets/images/index";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full outline-2 outline-dotted ">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Nike Logo" width={120} height={40} />
+        </a>
+
+        <ul className="flex flex-1 justify-center gap-16 max-lg:hidden ">
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <a href={link.href}>{link.label}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="hamburger-menu" width={30} height={40} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
