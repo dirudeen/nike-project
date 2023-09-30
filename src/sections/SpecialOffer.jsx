d1cb7fb9cb3625fa862adf31fa6560449956115d
Hadiru Colley <dirudeen22@gmail.com>
2023-09-30 17:51:47 +0300

    Add special offer image and its styling

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,10 +1,15 @@
+import { offer } from "../assets/images";
 
 
 const SpecialOffer = () => {
     return (
-        <div>
-            SpecialOffer
-        </div>
+        <section className="flex items-center max-xl:flex-col-reverse gap-10 max-container">
+            <div className="flex-1">
+                <img src={offer} height={687} width={773} alt="Special-Offer" className="object-contain" />
+            </div>
+
+            
+        </section>
     );
 }
 
