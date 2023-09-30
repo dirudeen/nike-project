337b3b656cbdbbda17fb2ead39a4cce09ad8e5b5
Hadiru Colley <dirudeen22@gmail.com>
2023-09-30 15:47:51 +0300

    create shoe component

---
+++ src/components/ShoeCard.jsx
@@ -0,0 +1,29 @@
+const ShoeCard = ({ imgURL, changeBigShoeImg, bigShoeImg }) => {
+  const clickHandler = () => {
+    if (bigShoeImg !== imgURL.bigShoe) {
+      changeBigShoeImg(imgURL.bigShoe);
+    }
+  };
+  return (
+    <div
+      className={`rounded-xl border-2 ${
+        bigShoeImg === imgURL.bigShoe
+          ? "border-coral-red"
+          : "border-transparent"
+      } cursor-pointer max-sm:flex`}
+      onClick={clickHandler}
+    >
+      <div className="flex items-center justify-center rounded-xl bg-coral-red bg-card bg-center max-sm:p-4 sm:h-40 sm:w-40">
+        <img
+          src={imgURL.thumbnail}
+          alt="shoe collection"
+          width={127}
+          height={103}
+          className="object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ShoeCard;
