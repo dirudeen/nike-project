1fea33551aaac5ec7b6aeb928744c1709084c654
Hadiru Colley <dirudeen22@gmail.com>
2023-09-30 17:12:36 +0300

    conditionally render the icon in the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ label, icon }) => {
   return (
     <button className="flex items-center justify-center gap-2 px-7 py-4 font-montserrat leading-none text-lg bg-coral-red rounded-full text-white ">
       {label}
-      <img src={icon} alt="arrow-right" className="h-5 w-5 rounded-full ml-2" />
+      {icon && <img src={icon} alt="arrow-right" className="h-5 w-5 rounded-full ml-2" />}
     </button>
   );
 };
