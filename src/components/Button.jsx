69c5bf8ab148b83fef9b42ba0767edcceb227364
Hadiru Colley <dirudeen22@gmail.com>
2023-09-30 18:27:25 +0300

    Add different styling to be conditionally render

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,18 @@
-const Button = ({ label, icon }) => {
+const Button = ({ label, icon, bgClr, borderClr, textClr }) => {
   return (
-    <button className="flex items-center justify-center gap-2 px-7 py-4 font-montserrat leading-none text-lg bg-coral-red rounded-full text-white ">
+    <button
+      className={`flex items-center justify-center gap-2 rounded-full px-7 py-4 font-montserrat text-lg  leading-none border-2
+    ${bgClr ? `${bgClr} ${borderClr} ${textClr}` : "bg-coral-red border-coral-red text-white"}
+      `}
+    >
       {label}
-      {icon && <img src={icon} alt="arrow-right" className="h-5 w-5 rounded-full ml-2" />}
+      {icon && (
+        <img
+          src={icon}
+          alt="arrow-right"
+          className="ml-2 h-5 w-5 rounded-full"
+        />
+      )}
     </button>
   );
 };
