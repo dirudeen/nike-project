fc8ab95c86e3e22a32f190999c8e9d8234f572a4
Hadiru Colley <dirudeen22@gmail.com>
2023-09-30 17:45:19 +0300

    modify the subtext property to subText

--- src/constants/index.js
+++ src/constants/index.js
@@ -80,18 +80,18 @@ export const services = [
   {
     imgURL: truckFast,
     label: "Free shipping",
-    subtext: "Enjoy seamless shopping with our complimentary shipping service.",
+    subText: "Enjoy seamless shopping with our complimentary shipping service.",
   },
   {
     imgURL: shieldTick,
     label: "Secure Payment",
-    subtext:
+    subText:
       "Experience worry-free transactions with our secure payment options.",
   },
   {
     imgURL: support,
     label: "Love to help you",
-    subtext: "Our dedicated team is here to assist you every step of the way.",
+    subText: "Our dedicated team is here to assist you every step of the way.",
   },
 ];
 
