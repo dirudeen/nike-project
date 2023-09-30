bd8195dbe90596443badff23fcf5d6dfa1f2ed31
Hadiru Colley <dirudeen22@gmail.com>
2023-09-30 11:56:58 +0300

    Add styles and content to the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,10 @@
-
-
-const Button = () => {
-    return <button className='' >
-        Click me
-    </button>;
-}
+const Button = ({ label, icon }) => {
+  return (
+    <button className="flex items-center justify-center gap-2 px-7 py-4 font-montserrat leading-none text-lg bg-coral-red rounded-full text-white ">
+      {label}
+      <img src={icon} alt="arrow-right" className="h-5 w-5 rounded-full ml-2" />
+    </button>
+  );
+};
 
 export default Button;
