8dfd1465a08d13fc15be888658bab8bdc0c74c1c
Hadiru Colley <dirudeen22@gmail.com>
2023-09-30 16:24:09 +0300

    Add content and styling

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,9 +1,17 @@
+import { star } from "../assets/icons";
 
 
-const PopularProductCard = () => {
+const PopularProductCard = ({imgURL, price, name ,rating}) => {
     return (
-        <div>
-            
+        <div className="flex flex-1 flex-col w-full max-sm:w-full">
+           <img 
+           src={imgURL} alt={name} className="w-[12.5rem] h-[12.5rem]" />
+           <div className="mt-8 flex justify-start gap-2.5">
+            <img src={star} alt="rating" width={24} height={24} />
+            <p className="text-slate-gray font-montserrat text-xl leading-normal">{rating}</p>
+           </div>
+           <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin ">{name}</h3>
+           <p className="mt-2 font-semibold font-montserrat text-2xl leading-normal text-coral-red">{price}</p>
         </div>
     );
 }
