4d022c0712ada622bb94086761cd642c89ec6bfd
Hadiru Colley <dirudeen22@gmail.com>
2023-09-30 17:45:45 +0300

    Add all neccesery content and styling

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,11 +1,35 @@
-
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
 
 const SuperQuality = () => {
-    return (
-        <div>
-           SuperQuality 
+  return (
+    <section
+      id="about-us"
+      className="max-container flex w-full items-center justify-between gap-10 max-lg:flex-col"
+    >
+      <div className="flex flex-col">
+        <h2 className="mt-10 font-palanquin text-4xl font-bold capitalize lg:max-w-lg">
+          We Provide You <span className="text-coral-red">Super Quality</span>{" "}
+          Shoes
+        </h2>
+        <p className="info-text mt-4 lg:max-w-lg">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your exprience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="info-text mt-6 lg:max-w-lg">
+          Our dedication to details and excellence ensures your satisfaction
+        </p>
+
+        <div className="mt-6">
+          <Button label="View details" icon={null} />
         </div>
-    );
-}
+      </div>
+      <div className=" flex flex-1 justify-center items-center">
+        <img src={shoe8} alt="Shoe8" width={570} height={522} className="object-contain"/>
+      </div>
+    </section>
+  );
+};
 
 export default SuperQuality;
