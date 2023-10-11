97726444959bb7854b312c3bd086655010e00f1b
Hadiru Colley <dirudeen22@gmail.com>
2023-10-11 17:37:10 +0300

    create ReviewCard.test.jsx

---
+++ src/__test__/ReviewCard.test.jsx
@@ -0,0 +1,11 @@
+import { Screen, render } from "@testing-library/react";
+import ReviewCard from "../components/ReviewCard";
+import {test, describe, expect} from "vitest"
+
+
+describe('Review card', () => {
+    test('should render correctly', () => {
+        
+    });
+    
+});
