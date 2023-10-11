08853decb05c407c8d96a0726ac7764e6d7cdaa0
Hadiru Colley <dirudeen22@gmail.com>
2023-10-11 18:08:10 +0300

    Add tests for rendering d component and with props

--- src/__test__/ReviewCard.test.jsx
+++ src/__test__/ReviewCard.test.jsx
@@ -1,11 +1,31 @@
-import { Screen, render } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import ReviewCard from "../components/ReviewCard";
-import {test, describe, expect} from "vitest"
+import { test, describe, expect } from "vitest";
 
+describe("Review card", () => {
+  test("should render correctly", () => {
+    render(<ReviewCard />);
+    const customerImgElement = screen.getByAltText(/customer/i);
+    expect(customerImgElement).toBeInTheDocument();
+  });
 
-describe('Review card', () => {
-    test('should render correctly', () => {
-        
-    });
-    
+  test("should render correctly with props ", () => {
+    render(
+      <ReviewCard
+        imgURL="https://via.placeholder.com/300.png/09f/fff"
+        customerName="Hadiru"
+        feedback="Good product"
+        rating="4.6"
+      />,
+    );
+
+    const starIconImgElement = screen.getByAltText(/star/i);
+    expect(starIconImgElement).toBeInTheDocument();
+
+    const ratingNumberElement = screen.getByText("4.6");
+    expect(ratingNumberElement).toHaveTextContent("4.6");
+
+    const customerNameElement = screen.getByRole("heading");
+    expect(customerNameElement).toBeInTheDocument();
+  });
 });
