c080c7ef5e75ceb6eaf5a54d7945b52e5525c262
Hadiru Colley <dirudeen22@gmail.com>
2023-10-11 18:37:48 +0300

    Add test for rendering props correctly

--- src/__test__/ServiceCard.test.jsx
+++ src/__test__/ServiceCard.test.jsx
@@ -1,7 +1,23 @@
 import { render, screen } from "@testing-library/react";
-import ServiceCard from "../components/ServiceCard"
+import ServiceCard from "../components/ServiceCard";
 import { test, describe, expect } from "vitest";
 
-describe('Service card', () => {
-    
+describe("Service card", () => {
+  test("should render correctly with props", () => {
+    render(
+      <ServiceCard
+        imgURL=" https://via.placeholder.com/300.png/09f/fff"
+        label="Free shipping"
+        subText="Some piece of subtext"
+      />,
+    );
+    const iconElement = screen.getByAltText("Free shipping");
+    expect(iconElement).toBeInTheDocument();
+
+    const labelElement = screen.getByRole("heading");
+    expect(labelElement).toBeInTheDocument();
+
+    const subTextElement = screen.getByText(/some piece of subtext/i);
+    expect(subTextElement).toBeInTheDocument();
+  });
 });
