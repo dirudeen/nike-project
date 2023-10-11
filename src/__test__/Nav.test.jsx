5e7c53314c823ac6c86635279a727a6898852276
Hadiru Colley <dirudeen22@gmail.com>
2023-10-11 13:55:26 +0300

    Add more test for nav component

--- src/__test__/Nav.test.jsx
+++ src/__test__/Nav.test.jsx
@@ -1,13 +1,24 @@
-import { render, screen } from "@testing-library/react"
-import Nav from "../components/Nav"
-import { describe, expect, test } from "vitest"
+import { render, screen } from "@testing-library/react";
+import Nav from "../components/Nav";
+import { describe, expect, test } from "vitest";
+import { navLinks } from "../constants";
 
-describe('Nav component', () => {
-    
-    test('should render correctly', () => {
-       render(<Nav />)
-       const headerElement = screen.getByRole("banner")
-       expect(headerElement).toBeInTheDocument() 
-    });
-    
+describe("Nav component", () => {
+  test("should render correctly", () => {
+    render(<Nav />);
+    const headerElement = screen.getByRole("banner");
+    expect(headerElement).toBeInTheDocument();
+
+    const navElement = screen.getByRole("navigation");
+    expect(navElement).toBeInTheDocument();
+
+    const logoImgElement = screen.getByAltText(/nike logo/i);
+    expect(logoImgElement).toBeInTheDocument();
+
+    const navItemElements = screen.getAllByRole("listitem");
+    expect(navItemElements).toHaveLength(navLinks.length);
+
+    const hamburgerIconElement = screen.getByAltText("hamburger-menu");
+    expect(hamburgerIconElement).toBeInTheDocument();
+  });
 });
