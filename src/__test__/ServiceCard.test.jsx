5cf1cccc8bb0b2543a61085a050e8be36808fa5c
Hadiru Colley <dirudeen22@gmail.com>
2023-10-11 18:12:18 +0300

    create ServiceCard.test.jsx

---
+++ src/__test__/ServiceCard.test.jsx
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import ServiceCard from "../components/ServiceCard"
+import { test, describe, expect } from "vitest";
+
+describe('Service card', () => {
+    
+});
