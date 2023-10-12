e0eaaa5e33e8922f5f615dfcfd6fa1f4f558d370
Hadiru Colley <dirudeen22@gmail.com>
2023-10-12 22:50:52 +0300

    create a custom hook that handles and manage theme

---
+++ src/hooks/useDark.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function useDark() {
+  const [theme, setTheme] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme ? savedTheme : "light";
+  });
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (theme === "light") {
+      root.classList.remove("light");
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+      root.classList.add("light");
+    }
+  }, [theme]);
+
+  return { toggleTheme };
+}
