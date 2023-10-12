a116d129eab67edf860d2e6356b44dfb3f238def
Hadiru Colley <dirudeen22@gmail.com>
2023-10-12 15:18:15 +0300

    Add state variable to render different classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from "react";
 import Nav from "./components/Nav";
 import {
   CustomerReviews,
@@ -10,42 +11,50 @@ import {
   Subscribe,
 } from "./sections/index";
 
+const App = () => {
+  const [sidebarIsActive, setSidebarIsActive] = useState(null);
 
-const App = () => (
-  <main className="relative">
-    <Nav />
-    <section className="xl:padding-l wide:padding-r padding-b">
-      <Hero />
-    </section>
 
-    <section className="padding">
-      <PopularProducts />
-    </section>
+  const toggleHander = useCallback((val) => {
+    setSidebarIsActive(val)
+  },[]);
 
-    <section className="padding">
-      <SuperQuality />
-    </section>
+  return (
+    <main className={` ${!sidebarIsActive ? "fixed overflow-hidden inset-0": "relative"}`}>
+      <Nav onToggle={toggleHander} />
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
 
-    <section className="padding-x py-10">
-      <Services />
-    </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
 
-    <section className="padding">
-      <SpecialOffer />
-    </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
 
-    <section className="bg-pale-blue padding">
-      <CustomerReviews />
-    </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
 
-    <section className="padding-x sm:py-32 py-16 w-full">
-      <Subscribe />
-    </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
 
-    <section className="bg-black padding-x padding-t pb-8">
-      <Footer />
-    </section>
-  </main>
-);
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+
+      <section className="padding-x w-full py-16 sm:py-32">
+        <Subscribe />
+      </section>
+
+      <section className="padding-x padding-t bg-black pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
+};
 
 export default App;
