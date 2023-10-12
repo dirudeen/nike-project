f6d82596b76f1636cbbba709f92394bb5519c66b
Hadiru Colley <93408077+dirudeen@users.noreply.github.com>
2023-10-12 23:28:46 +0300

    modify the initialRender to state variable

--- src/hooks/useDark.jsx
+++ src/hooks/useDark.jsx
@@ -5,23 +5,25 @@ export default function useDark() {
     const savedTheme = localStorage.getItem("theme");
     return savedTheme ? savedTheme : "light";
   });
-
+  const[initialRender, setInitialRender] = useState(false);
+  
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
+    setInitialRender(true)
     localStorage.setItem("theme", newTheme);
   };
-  let initialRender = 1
+  
   useEffect(() => {
     const root = window.document.documentElement;
-    if (theme === "light" && initialRender === 2) {
+    if (theme === "light" && initialRender) {
       root.classList.remove("light");
       root.classList.add("dark");
     } else {
       root.classList.remove("dark");
       root.classList.add("light");
     }
-    initialRender = 2
+    
   }, [theme, initialRender]);
 
   return { toggleTheme };
