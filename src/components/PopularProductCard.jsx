b6841a17940ffa797f948e2afc50426ba5950fc3
Hadiru Colley <dirudeen22@gmail.com>
2023-10-13 16:35:00 +0300

    Add text-slate-light-gery color to p elements

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -8,7 +8,7 @@ const PopularProductCard = ({imgURL, price, name ,rating}) => {
            src={imgURL} alt={name} className="w-[12.5rem] h-[12.5rem]" />
            <div className="mt-8 flex justify-start gap-2.5">
             <img src={star} alt="rating" width={24} height={24} />
-            <p className="text-slate-gray font-montserrat text-xl leading-normal">{rating}</p>
+            <p className="text-slate-gray font-montserrat text-xl leading-normal dark:text-slate-light-gray">{rating}</p>
            </div>
            <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin ">{name}</h3>
            <p className="mt-2 font-semibold font-montserrat text-2xl leading-normal text-coral-red">{price}</p>
