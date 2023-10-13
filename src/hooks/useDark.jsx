731d75e07782a64e60853a94299a33b3438f8b53
Hadiru Colley <dirudeen22@gmail.com>
2023-10-13 11:57:30 +0300

    switch if statement bodys

--- src/hooks/useDark.jsx
+++ src/hooks/useDark.jsx
@@ -5,26 +5,25 @@ export default function useDark() {
     const savedTheme = localStorage.getItem("theme");
     return savedTheme ? savedTheme : "light";
   });
-  const[initialRender, setInitialRender] = useState(false);
+
   
   const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
-    setInitialRender(true)
     localStorage.setItem("theme", newTheme);
   };
   
   useEffect(() => {
     const root = window.document.documentElement;
-    if (theme === "light" && initialRender) {
-      root.classList.remove("light");
-      root.classList.add("dark");
-    } else {
+    if (theme === "light") {
       root.classList.remove("dark");
       root.classList.add("light");
+    } else {
+      root.classList.remove("light");
+      root.classList.add("dark");
     }
     
-  }, [theme, initialRender]);
+  }, [theme]);
 
   return { toggleTheme };
 }
