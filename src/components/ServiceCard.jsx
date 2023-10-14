06c24213d4c70ba65a36dc23a4e5d3570404dfca
Hadiru Colley <dirudeen22@gmail.com>
2023-10-14 13:17:20 +0300

    Add box shadow for dark theme

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -2,7 +2,7 @@
 
 const ServiceCard = ({imgURL, label, subText}) => {
     return (
-      <div className="w-full flex-1 rounded-[1.35rem] px-10 py-16 shadow-3xl dark:bg-slate-900 dark:text-white  sm:w-[21.875] sm:min-w-[21.875]">
+      <div className="w-full flex-1 rounded-[1.35rem] px-10 py-16 shadow-3xl dark:shadow-dark dark:bg-slate-900 dark:text-white  sm:w-[21.875] sm:min-w-[21.875]">
         <div className="h-11 w-11 items-center justify-center rounded-full bg-coral-red">
           <img src={imgURL} alt={label} height={24} width={24} />
         </div>
