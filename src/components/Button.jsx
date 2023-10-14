6d0ddf4c85e8013a223f6f9ff4411c19ef509904
Hadiru Colley <dirudeen22@gmail.com>
2023-10-14 16:45:53 +0300

    Add hover, active and focus state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,16 @@
+import {motion} from "framer-motion"
+
 const Button = ({ label, icon, bgClr, borderClr, textClr, fullWidth }) => {
   return (
-    <button
-      className={`flex items-center justify-center gap-2 rounded-full px-7 py-4 font-montserrat text-lg  leading-none border-2 ${fullWidth && "w-full"}
+    <motion.button
+      className={`flex items-center
+      outline-none
+      justify-center gap-2 rounded-full px-7 py-4 font-montserrat text-lg   leading-none border-2 ${fullWidth && "w-full"}
     ${bgClr ? `${bgClr} ${borderClr} ${textClr}` : "bg-coral-red border-coral-red text-white"}
       `}
+      whileHover={{scale: 1.1}}
+      whileTap={{scale: 1.05}}
+      whileFocus={{border: "2px solid white"}}
     >
       {label}
       {icon && (
@@ -13,7 +20,7 @@ const Button = ({ label, icon, bgClr, borderClr, textClr, fullWidth }) => {
           className="ml-2 h-5 w-5 rounded-full"
         />
       )}
-    </button>
+    </motion.button>
   );
 };
 
