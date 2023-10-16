a24700daaf963733d549c02d4d31a97f1ca62a81
Hadiru Colley <dirudeen22@gmail.com>
2023-10-16 22:08:42 +0300

    Format with pritter

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,15 +1,14 @@
 import ServiceCard from "../components/ServiceCard";
 import { services } from "../constants";
 
-
 const Services = () => {
-    return (
-        <section className="max-container flex justify-center flex-wrap gap-9">
-            {services.map(service => (
-                <ServiceCard key={service.label} {...service} />
-            ))}
-        </section>
-    );
-}
+  return (
+    <section className="max-container flex flex-wrap justify-center gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
+};
 
 export default Services;
