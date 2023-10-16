53cac2e50a25340436389e478d6b8b16f86755c7
Hadiru Colley <dirudeen22@gmail.com>
2023-10-16 18:30:34 +0300

    Add animation for when in view

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,19 +1,44 @@
+import { motion } from "framer-motion";
 import { star } from "../assets/icons";
+import useScrollReveal from "../hooks/useScrollReveal";
 
-
-const PopularProductCard = ({imgURL, price, name ,rating}) => {
-    return (
-        <div className="flex flex-1 flex-col w-full max-sm:w-full">
-           <img 
-           src={imgURL} alt={name} className="w-[12.5rem] h-[12.5rem]" />
-           <div className="mt-8 flex justify-start gap-2.5">
-            <img src={star} alt="rating" width={24} height={24} />
-            <p className="text-slate-gray font-montserrat text-xl leading-normal dark:text-slate-light-gray">{rating}</p>
-           </div>
-           <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin ">{name}</h3>
-           <p className="mt-2 font-semibold font-montserrat text-2xl leading-normal text-coral-red">{price}</p>
-        </div>
-    );
+const popularCardVariant = {
+    hidden: {
+        scale: 0.5,
+        rotate: -30,
+        opacity: 0
+    },
+    visible: {
+        scale: 1,
+        opacity: 1,
+        rotate: 0,
+        transition: {duration: 0.8, easings: "easeOut"}
+    }
 }
+
+const PopularProductCard = ({ imgURL, price, name, rating }) => {
+    const {ref, control} = useScrollReveal()
+  return (
+    <motion.div className="flex w-full flex-1 flex-col max-sm:w-full"
+    variants={popularCardVariant}
+    initial="hidden"
+    animate={control}
+    ref={ref}>
+      <img src={imgURL} alt={name} className="h-[12.5rem] w-[12.5rem]" />
+      <div className="mt-8 flex justify-start gap-2.5">
+        <img src={star} alt="rating" width={24} height={24} />
+        <p className="font-montserrat text-xl leading-normal text-slate-gray dark:text-slate-light-gray">
+          {rating}
+        </p>
+      </div>
+      <h3 className="mt-2 font-palanquin text-2xl font-semibold leading-normal ">
+        {name}
+      </h3>
+      <p className="mt-2 font-montserrat text-2xl font-semibold leading-normal text-coral-red">
+        {price}
+      </p>
+    </motion.div>
+  );
+};
 
 export default PopularProductCard;
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,24 +1,52 @@
+import { motion } from "framer-motion";
 import PopularProductCard from "../components/PopularProductCard";
 import { products } from "../constants";
+import useScrollReveal from "../hooks/useScrollReveal";
+
+
+const popularTextVariants = {
+  hidden: {
+    y: 50,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: { delay: 0.5, duration: 1, easings: "easeOut"},
+  },
+};
+
 
 
 
 const PopularProducts = () => {
-    return (
-        <section id="products" className="max-container max-sm:mt-12">
-            <div className="flex flex-col justify-start gap-5">
-                <h2 className="text-4xl font-palanquin font-bold">Our <span className="text-coral-red">Popular</span> Projucts</h2>
-                <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray dark:text-slate-light-gray">Exprience top-notch quality and style with our sought-after selections. Discover a world of comfort, design and value</p>
-            </div>
+    const {ref, control} = useScrollReveal()
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <motion.div className="flex flex-col justify-start gap-5"
+      variants={popularTextVariants}
+      initial="hidden"
+      animate={control}
+      ref={ref}
+      >
 
-            <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14">
-                {products.map(product => (
-                    <PopularProductCard
-                    key={product.name} {...product} />
-                ))}
-            </div>
-        </section >
-    );
-}
+        <h2 className="font-palanquin text-4xl font-bold">
+          Our <span className="text-coral-red">Popular</span> Projucts
+        </h2>
+        <p className="mt-2 font-montserrat text-slate-gray dark:text-slate-light-gray lg:max-w-lg">
+          Exprience top-notch quality and style with our sought-after
+          selections. Discover a world of comfort, design and value
+        </p>
+      </motion.div>
+
+      <div className="mt-16 grid grid-cols-1 gap-14 sm:grid-cols-2 sm:gap-6 md:grid-cols-3 lg:grid-cols-4"
+      >
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default PopularProducts;
