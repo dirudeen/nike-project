895b146ca090cbe4fbe87d348a3a2b22ac032fa6
Hadiru Colley <dirudeen22@gmail.com>
2023-10-16 22:06:57 +0300

    Add reveal on scroll animation

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,16 +1,43 @@
+import { motion } from "framer-motion";
 import ReviewCard from "../components/ReviewCard";
 import { reviews } from "../constants";
+import useScrollReveal from "../hooks/useScrollReveal";
+
+const textVariant = {
+  hidden: {
+    y: 50,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: { duration: 1, ease: "easeOut" },
+  },
+};
 
 const CustomerReviews = () => {
+  const { ref, control } = useScrollReveal();
   return (
     <section className="max-container">
-      <h3 className="text-center font-palanquin text-4xl font-bold">
+      <motion.h3
+        className="text-center font-palanquin text-4xl font-bold"
+        variants={textVariant}
+        animate={control}
+        initial="hidden"
+        ref={ref}
+      >
         What Our <span className="text-coral-red">Customers</span>Says?
-      </h3>
-      <p className="info-text dark:text-slate-light-gray m-auto mt-4 max-w-lg  text-center">
+      </motion.h3>
+      <motion.p
+        className="info-text m-auto mt-4 max-w-lg text-center  dark:text-slate-light-gray"
+        variants={textVariant}
+        animate={control}
+        initial="hidden"
+        ref={ref}
+      >
         Hear genuine stories from our satisfied customers about their
         exceptional experiences with us
-      </p>
+      </motion.p>
 
       <div className="mt-24 flex flex-1 items-center justify-evenly gap-14 max-lg:flex-col">
         {reviews.map((review) => (
