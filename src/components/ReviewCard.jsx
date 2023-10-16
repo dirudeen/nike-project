24247e0e6720ef351e03c0b5f6f3bc1e0a773982
Hadiru Colley <dirudeen22@gmail.com>
2023-10-16 22:06:30 +0300

    Add reveal on scroll animation

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,8 +1,33 @@
+import { motion } from "framer-motion";
 import { star } from "../assets/icons";
+import useScrollReveal from "../hooks/useScrollReveal";
+
+const containerVariant = {
+  hidden: {
+    scale: 0,
+    opacity: 0,
+  },
+  visible: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      delay: 0.7,
+      type: "spring",
+      stiffness: 300,
+      damping: 20,
+    },
+  },
+};
 
 const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
+  const {ref, control} = useScrollReveal()
   return (
-    <div className="flex flex-col items-center justify-center dark:bg-slate-900 dark:text-white">
+    <motion.div className="flex flex-col items-center justify-center dark:bg-slate-900 dark:text-white"
+    variants={containerVariant}
+    animate={control}
+    initial="hidden"
+    ref={ref}
+    >
       <img
         src={imgURL}
         alt="customer"
@@ -27,7 +52,7 @@ const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
       <h3 className="mt-1 text-center font-palanquin text-3xl font-bold">
         {customerName}
       </h3>
-    </div>
+    </motion.div>
   );
 };
 
