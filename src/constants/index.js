eb179819c405d96af7c6610bc8567383566dc968
Hadiru Colley <dirudeen22@gmail.com>
2023-10-18 19:43:31 +0300

    Add ids for each shoe object

--- src/constants/index.js
+++ src/constants/index.js
@@ -32,14 +32,17 @@ export const shoes = [
   {
     thumbnail: thumbnailShoe1,
     bigShoe: bigShoe1,
+    id: "shoe 1"
   },
   {
     thumbnail: thumbnailShoe2,
     bigShoe: bigShoe2,
+    id: "shoe 2"
   },
   {
     thumbnail: thumbnailShoe3,
     bigShoe: bigShoe3,
+    id: "shoe 3"
   },
 ];
 
