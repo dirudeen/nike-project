92fead892a5200cc40c12d1e957e43a0bfe82501
Hadiru Colley <dirudeen22@gmail.com>
2023-10-19 17:16:14 +0300

    Add test for rendering each review card

--- src/__test__/CustomerReviews.test.jsx
+++ src/__test__/CustomerReviews.test.jsx
@@ -2,15 +2,37 @@ import { screen, render } from "@testing-library/react";
 import "../mocks/intersectionObserverMock";
 import { test, describe, expect } from "vitest";
 import { CustomerReviews } from "../sections";
+import { reviews } from "../constants";
 
 describe("Customer Reviews - Component", () => {
+  test("should render component correctly", () => {
+    render(<CustomerReviews />);
 
-    test('should render component correctly', () => {
-        render(<CustomerReviews />);
-
-        const headingElement = screen.getByRole("heading",{name: "What Our Customers Says?"})
-        expect(headingElement).toBeInTheDocument();
-        expect(headingElement).toHaveTextContent(/What our customers says\?/i);
+    const headingElement = screen.getByRole("heading", {
+      name: "What Our Customers Says?",
     });
-    
-})
+    expect(headingElement).toBeInTheDocument();
+    expect(headingElement).toHaveTextContent(/What our customers says\?/i);
+
+    const paragraphElement = screen.getByText(/Hear.*us\./i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  test("should render 3 review cards correctly", () => {
+    render(<CustomerReviews />);
+
+    const reviewCards = screen.getAllByTestId("review-card");
+    expect(reviewCards).toHaveLength(reviews.length);
+
+    reviewCards.forEach((card) => {
+      expect(card).toBeInTheDocument();
+    });
+
+    const customerImages = screen.getAllByAltText("customer");
+
+    customerImages.forEach((image, i) => {
+      expect(image).toHaveAttribute("src", `${reviews[i].imgURL}`);
+    });
+  });
+  
+});
