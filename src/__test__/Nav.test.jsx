f6cd03e040c967af99d00365f57a7ff94323a2bf
Hadiru Colley <dirudeen22@gmail.com>
2023-10-19 20:35:46 +0300

    Add test for sidebar menu functioinality

--- src/__test__/Nav.test.jsx
+++ src/__test__/Nav.test.jsx
@@ -2,6 +2,8 @@ import { render, screen } from "@testing-library/react";
 import Nav from "../components/Nav";
 import { describe, expect, test } from "vitest";
 import { navLinks } from "../constants";
+import userEvent from "@testing-library/user-event";
+
 
 describe("Nav component", () => {
   test("should render correctly", () => {
@@ -18,7 +20,22 @@ describe("Nav component", () => {
     const navItemElements = screen.getAllByRole("listitem");
     expect(navItemElements).toHaveLength(navLinks.length);
 
+    navItemElements.forEach((navItemElement) => {
+      expect(navItemElement).toBeInTheDocument();
+    })
+
     const hamburgerIconElement = screen.getByAltText("hamburger-menu");
     expect(hamburgerIconElement).toBeInTheDocument();
   });
+
+  test('ul element in nav should have the class sidebar-Out when hamburger-menu is clicked', async() => {
+    render(<Nav onToggle={() => {}}/>);
+    userEvent.setup()
+    const navListElement = screen.getByRole("list");
+    expect(navListElement).toHaveClass("sidebar-In");
+    const hamburgerIconElement = screen.getByRole("button");
+    await userEvent.click(hamburgerIconElement);
+    expect(navListElement).toHaveClass("sidebar-Out");
+  });
+  
 });
